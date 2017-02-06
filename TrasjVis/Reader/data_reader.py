5508337c1cecbb7ff7fc2d04f291685c8c454e4d
Gregorio Palmas <gpalmas@kth.se>
2017-02-06 13:48:50 +0200

    first commit

---
+++ TrasjVis/Reader/data_reader.py
@@ -0,0 +1,7 @@
+
+import numpy as np
+
+def GetData(path):
+   data = np.genfromtxt(path, dtype = None, delimiter = ",", names = True)
+   
+   return data
---
+++ TrasjVis/Reader/debug.py
@@ -0,0 +1,4 @@
+
+import data_reader as dr
+
+data = dr.GetData("C:/Projects/AnttiTraj/data/eye-tracking.txt")
